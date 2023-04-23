4a1503ed6e3c3c1b6461207be39662f00ba4680a
Konrad Gnat <konradmgnat@gmail.com>
2023-04-23 17:58:43 +0700

    implement header display

--- packages/nextjs/components/diamond/DiamondHeader.tsx
+++ packages/nextjs/components/diamond/DiamondHeader.tsx
@@ -14,7 +14,7 @@ export const DiamondHeader = () => {
   return (
     <div className="min-h-0 p-5 mb-11 lg:mb-0">
       <div>
-        <div className="fixed flex justify-between items-center w-full z-20 p-4 top-15 left-0 pointer-events-none justify-end">
+        <div className="fixed flex items-center w-full z-20 p-4 top-15 right-0 pointer-events-none justify-end">
           <div className="flex space-x-2 pointer-events-auto flex-col bg-base-100 px-4 py-4 text-center items-center rounded-3xl">
             <span className="font-bold">{mainDiamondContractName}</span>
             <Address address={deployedContractLoading ? "..." : deployedContractData?.address} />
